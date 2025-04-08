4535c4d9c9c6d85256b23c220753ffeea8455e3e
Chris Saunders <christophersaunders@comcast.net>
2025-04-08 17:30:19 -0400

    Update M1_test.ps1
    Removed formatting to show source

--- M1_test.ps1
+++ M1_test.ps1
@@ -1,7 +1,7 @@
-Test-Connection -Count 1 champlain.edu | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
-Test-Connection -Count 1 fw-caw.caw.local | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
-Test-Connection -Count 1 dc1-caw.caw.local | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
-Test-Connection -Count 1 dc2-caw.caw.local | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
-Test-Connection -Count 1 w1-caw.caw.local | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
-Test-Connection -Count 1 w2-caw.caw.local | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
-Test-Connection -Count 1 mgmt1-caw.caw.local | Select-Object @{Name="Target";Expression={$_.Address}}, @{Name="Latency (ms)";Expression={$_.ResponseTime}}, @{Name="Status";Expression={$_.StatusCode}}
+Test-Connection -Count 1 champlain.edu
+Test-Connection -Count 1 fw-caw.caw.local
+Test-Connection -Count 1 dc1-caw.caw.local
+Test-Connection -Count 1 dc2-caw.caw.local
+Test-Connection -Count 1 w1-caw.caw.local
+Test-Connection -Count 1 w2-caw.caw.local
+Test-Connection -Count 1 mgmt1-caw.caw.local
